25ad51db90bc5e3392cec410cebb48098eadb231
Juti <oskarjut@metropolia.fi>
2024-10-01 10:34:26 +0300

    lisätty sattumatapahtuman hakeva funktio

---
+++ main.py
@@ -0,0 +1,11 @@
+import csv
+import random
+
+def pick_random_event():
+    events = []
+    with open('events.csv', 'r', encoding='utf-8') as file:
+        csvfile = csv.reader(file)
+        for row in csvfile:
+            events.append(row)
+        event = events[random.randint(0, len(events) - 1)]
+        return {"type": event[0], "string": event[1]}
