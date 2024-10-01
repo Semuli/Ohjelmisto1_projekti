d50303acf0170821e9e0c86aa594183bda915334
Akseli Kiviruusu <akselikiviruusu@gmail.com>
2024-10-01 12:55:10 +0300

    tämänhetkisten pisteiden haku ja sql_connector(auki)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv
 import random
+import mysql.connector
+import config
 
 # Sattumatapahtumien satunnaishaku
 def pick_random_event():
@@ -10,6 +12,20 @@ def pick_random_event():
             events.append(row)
         event = events[random.randint(0, len(events) - 1)]
         return {"type": event[0], "string": event[1]}
+
+# hakee tämänhetkiset pisteet (kunhan ne on päivitetty tietokantaan oikein)
+def get_current_points_by_screen_name(screen_name):
+    sql = f"""
+    select points from game where screen_name = "{screen_name}";
+    """
+    print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if len(tulos) > 0:
+        print(f'Pisteesi tällä hetkellä {tulos[0]}')
+    else: # tämä on ehkä turha
+        print('Pisteesi tällä hetkellä: 0')
 
 def update_game_points(points):
     print("dapdap")
@@ -27,3 +43,12 @@ def update_game_current_location(newLocation):
 def distance_between_airfields(airfield1, airfield2):
     print("dapetidap")
 
+# sql yhteys ???
+yhteys = mysql.connector.connect(
+         host = '127.0.0.1',
+         port = 3306,
+         database = 'demo_game',
+         user = config.user,
+         password = config.password,
+         autocommit = True
+         )
