f6460965f891830c48f3a8e3b488a20a76a6347f
Juti <oskarjut@metropolia.fi>
2024-10-01 11:58:31 +0300

    lisätty jutin skeletonifunktioita

--- main.py
+++ main.py
@@ -10,3 +10,20 @@ def pick_random_event():
             events.append(row)
         event = events[random.randint(0, len(events) - 1)]
         return {"type": event[0], "string": event[1]}
+
+def update_game_points(points):
+    print("dapdap")
+
+def update_game_trophy(trophy):
+    print("dapdap")
+    # lista?
+
+def update_game_distance_travelled(distance):
+    print("dap")
+
+def update_game_current_location(newLocation):
+    print("dap")
+
+def distance_between_airfields(airfield1, airfield2):
+    print("dapetidap")
+
